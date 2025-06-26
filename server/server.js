711d0f63f08d3e86813068fe03b0fbfb2f824e88
Denis-alt254 <deniskipkurui2024@gmail.com>
2025-06-26 13:22:51 +0300

    connect mongodb

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
-const express = require('express')
+const express = require('express');
+const connectDB = require('./config/db');
+require('dotenv').config();
 
-const app = express()
-PORT = 5000
+const app = express();
+connectDB();
+PORT = 5000;
 
 app.listen(process.env.PORT || PORT, (req, res) =>{
     console.log(`Server is running at http://localhost:${PORT}`);
-})
+});
