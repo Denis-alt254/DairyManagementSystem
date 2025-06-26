841a4ce3042f642b464c52d7ab76bc6ac0de5249
Denis-alt254 <deniskipkurui2024@gmail.com>
2025-06-26 18:28:09 +0300

    Updated server.js

--- server/server.js
+++ server/server.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
+app.use(cors());
 connectDB();
+app.use(express.json());
 PORT = 5000;
+
+//Routes
+app.use('/api/auth', require('./routes/authRoutes'));
+app.use('/api/cows', require('./routes/cowRoutes'));
+app.use('/api/expenses', require('./routes/expenseRoutes'));
+app.use('/api/milk', require('./routes/milkRoutes'));
+app.use('/api/tasks', require('./routes/taskRoutes'));
 
 app.listen(process.env.PORT || PORT, (req, res) =>{
     console.log(`Server is running at http://localhost:${PORT}`);
