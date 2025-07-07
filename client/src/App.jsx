c6c401bbd75ed717895a445a4dec2a9bcb0547a2
Denis-alt254 <deniskipkurui2024@gmail.com>
2025-07-07 09:18:14 +0300

    updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,30 +1,41 @@
 import Navbar from "./components/Navbar";
-import Tasks, { AddTasksForm } from "./pages/Tasks";
+import Tasks, { AddTasksForm, UpdateAndDeleteTask } from "./pages/Tasks";
 import { Routes, Route } from 'react-router-dom';
 import './App.css'
 import {Cows, AddCowForm, UpdateAndDeleteForm} from "./pages/Cows";
 import { Dashboard } from "./pages/Dashboard";
-import { AddExpenseFrom, Expenses } from "./pages/Expenses";
+import { AddExpenseFrom, Expenses, Summary } from "./pages/Expenses";
 import { AddMilkRecordForm, Milk } from "./pages/Milk";
+import Footer from "./components/Footer";
+import SideBar from "./components/Sidebar";
 
 function App(){
   return(
-    <div className="background">
-      <Navbar />
-      <main>
-        <Routes>
-          <Route path="/" element = {<Cows />}></Route>
-          <Route path="/addcow" element={<AddCowForm />}/>
-          <Route path="/updatedelete" element={<UpdateAndDeleteForm />}/>
-          <Route path="/tasks" element={<Tasks />}/>
-          <Route path="/addtask" element={<AddTasksForm />}/>
-          <Route path="/dashboard" element={<Dashboard />}/>
-          <Route path="/addexpense" element={<AddExpenseFrom />}/>
-          <Route path="/expenses" element={<Expenses />}/>
-          <Route path="/milk" element={<Milk />}/>
-          <Route path="/addmilk" element={<AddMilkRecordForm />}/>
-        </Routes>
-      </main>
+    <div className="flex background">
+      <div className="sideBar">
+        <SideBar />
+      </div>
+      <div className="main">
+        <Navbar />
+        <main>
+          <Routes>
+            <Route path="/" element = {<Cows />}></Route>
+            <Route path="/addcow" element={<AddCowForm />}/>
+            <Route path="/updatedelete" element={<UpdateAndDeleteForm />}/>
+            <Route path="/tasks" element={<Tasks />}/>
+            <Route path="/addtask" element={<AddTasksForm />}/>
+            <Route path="/updatedeletetasks" element={<UpdateAndDeleteTask />}/>
+            <Route path="/dashboard" element={<Dashboard />}/>
+            <Route path="/addexpense" element={<AddExpenseFrom />}/>
+            <Route path="/expenses" element={<Expenses />}/>
+            <Route path="/summary" element={<Summary />}/>
+            <Route path="/milk" element={<Milk />}/>
+            <Route path="/addmilk" element={<AddMilkRecordForm />}/>
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+      
     </div>
   )
 }
