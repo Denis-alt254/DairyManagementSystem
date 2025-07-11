a875cf481b2d5f50b973b309dfb3f4fda16b7922
Denis-alt254 <deniskipkurui2024@gmail.com>
2025-07-11 15:42:47 +0300

    modify api

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: 'http://localhost:5000',
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+  timeout: 5000, // optional: set request timeout
 });
 
 export default API;
